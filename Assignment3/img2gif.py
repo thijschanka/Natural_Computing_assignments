c1c1d701b2c341825c10563a90f895fddbb63320
mschoppema <thijs@DESKTOP-D4KC0KF.localdomain>
2022-03-15 15:18:05 +0100

    added parameter handling

--- Assignment3/img2gif.py
+++ Assignment3/img2gif.py
@@ -1,29 +1,61 @@
 #from PIL import Image
 import imageio
+import pathlib
 import glob
 import os
 import time
+import click
 
-dir_name = 'assignment3_outputs//high_bc_bg//'
-fp_out = "assignment3_outputs//high_bc_bg_{}.gif"
-fps = 60
-save_each_files = 144
+@click.command()
+@click.option(
+    "--fp_in",
+    type=pathlib.Path,
+    required=True,
+    help="Path to the folder containing the images",
+)
+@click.option(
+    "--fp_out",
+    type=pathlib.Path,
+    required=True,
+    help="Path to the output_files",
+)
+@click.option(
+    "--fps",
+    type=int,
+    required=False,
+    default=60,
+    help="Path to the folder containing the images",
+)
+@click.option(
+    "--save_each_files",
+    type=int,
+    required=False,
+    default=144,
+    help="Increment to save a gif each N files, if 0 only the last will be saved",
+)
+def main(fp_in, fp_out, fps, save_each_files):
 
-# List files
-list_of_files = filter( os.path.isfile,
-                        glob.glob(dir_name + '*') )
-# Sort list of files based on last modification time in ascending order
-filenames = sorted( list_of_files,
-                        key = os.path.getmtime)
+    fp_in = fp_in / '*'
 
-# Parse files
-images = []
-for i, filename in enumerate(filenames):
-    images.append(imageio.imread(filename))
-    if i % save_each_files == 0:
-        imageio.mimsave(fp_out.format(str(i)), images, fps=fps)
-        
-# N seconds
-print(len(filenames), len(filenames) // fps)
+    # List files
+    list_of_files = filter( os.path.isfile,
+                            glob.glob(str(fp_in)) )
+    # Sort list of files based on last modification time in ascending order
+    filenames = sorted( list_of_files,
+                            key = os.path.getmtime)
 
-imageio.mimsave(fp_out.format("all"), images, fps=fps)
+    # Parse files
+    images = []
+    for i, filename in enumerate(filenames):
+        images.append(imageio.imread(filename))
+        if save_each_files != 0 and i % save_each_files == 0:
+            file_out = str(fp_out) + "_" + str(i) + '.gif'
+            imageio.mimsave(file_out, images, fps=fps)
+            
+    # N seconds
+    print(len(filenames), len(filenames) // fps)
+    file_out = str(fp_out) + "_all" + '.gif'
+    imageio.mimsave(file_out, images, fps=fps)
+
+if __name__ == "__main__":
+    main()
