08dddfc7ca94cf3305f8f5b4e6e0a10d07530495
Gerhard van der Knijff <47530488+gknife@users.noreply.github.com>
2022-03-17 15:39:40 +0100

    Create exercise1.py

---
+++ Assignment4/exercise1.py
@@ -0,0 +1,31 @@
+import numpy as np
+from sklearn import metrics
+from matplotlib import pyplot
+
+anomalyScores = []
+classes = []
+
+with open("c:/Users/gerha/OneDrive/Natural Computing/Assignment 4/negative-selection/output.txt") as english_file:
+    englishScores = english_file.readlines()
+    for score in englishScores:
+        if not score.isspace():
+            classes.append(0)
+            anomalyScores.append(float(score))
+
+
+with open("c:/Users/gerha/OneDrive/Natural Computing/Assignment 4/negative-selection/output2.txt") as anomaly_file:
+    otherLanguageScores = anomaly_file.readlines()
+    for score in otherLanguageScores:
+        if not score.isspace():
+            classes.append(1)
+            anomalyScores.append(float(score))
+
+fpr, tpr, thresholds = metrics.roc_curve(classes, anomalyScores, pos_label=1)
+
+auc = metrics.roc_auc_score(classes, anomalyScores)
+
+pyplot.plot(fpr, tpr, linestyle='--', label="Roc curve (AUC = " + str(auc) + ")")
+pyplot.xlabel("Specificity")
+pyplot.ylabel("Sensitivity")
+pyplot.legend()
+pyplot.show()
